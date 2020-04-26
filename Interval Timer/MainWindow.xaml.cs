c5945d4d6cdf9291ccb123e6f6bbb1a385e94cf9
MrHighTech <44378130+MrHighTech@users.noreply.github.com>
2020-04-26 14:06:07 +0200

    Add project files.

---
+++ Interval Timer/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Diagnostics;
+using System.Windows.Controls.Primitives;
+
+namespace Interval_Timer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer dt = new DispatcherTimer();
+        Stopwatch sw = new Stopwatch();
+        string currentTime = string.Empty;
+        public MainWindow()
+        {
+            InitializeComponent();
+            dt.Tick += new EventHandler(Dt_Tick);
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
+        }
+        private void ButtonSettings_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ButtonCallendar_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+        private void Shutdown_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void ButtonCloseMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Visible;
+            ButtonCloseMenu.Visibility = Visibility.Collapsed;
+        }
+
+        private void ButtonOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonOpenMenu.Visibility = Visibility.Collapsed;
+            ButtonCloseMenu.Visibility = Visibility.Visible;
+        }
+
+        private void Dt_Tick(object sender, EventArgs e)
+        {
+            if (sw.IsRunning)
+            {
+                TimeSpan ts = sw.Elapsed;
+                currentTime = String.Format("{0:00}:{1:00}:{2:00}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                Stopwatch.Text = currentTime;
+            }
+            else
+            {
+                Stopwatch.Text = "{Binding currentTime, StringFormat={}";
+            }
+        }
+
+        int clicker = 0;
+        private void StartStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (clicker % 2 != 0)
+            {
+                sw.Stop();
+                dt.Stop();
+                StartStop.Content = "Resume";
+            }
+            else
+            {
+                sw.Start();
+                dt.Start();
+                StartStop.Content = "Pause";
+            }
+            clicker++;
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+        private void ListViewMenu_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int index = ListViewMenu.SelectedIndex;
+            MoveCursorMenu(index);
+        }
+
+        private void MoveCursorMenu(int index)
+        {
+            TransitioningContentSlide.OnApplyTemplate();
+            GridCursor.Margin = new Thickness(0, 60 * index, 0, 0);
+        }
+    }
+}

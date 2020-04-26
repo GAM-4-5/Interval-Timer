65bcfc41dded39aff8069a42c1c7e2a28e5b1908
MrHighTech <44378130+MrHighTech@users.noreply.github.com>
2020-04-26 19:48:07 +0200

    update3

--- Interval Timer/MainWindow.xaml.cs
+++ Interval Timer/MainWindow.xaml.cs
@@ -15,6 +15,7 @@ using System.Windows.Shapes;
 using System.Windows.Threading;
 using System.Diagnostics;
 using System.Windows.Controls.Primitives;
+using Interval_Timer.Properties;
 
 namespace Interval_Timer
 {
@@ -23,14 +24,9 @@ namespace Interval_Timer
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer dt = new DispatcherTimer();
-        Stopwatch sw = new Stopwatch();
-        string currentTime = string.Empty;
         public MainWindow()
         {
             InitializeComponent();
-            dt.Tick += new EventHandler(Dt_Tick);
-            dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
         }
         private void ButtonSettings_Click(object sender, RoutedEventArgs e)
         {
@@ -60,46 +56,6 @@ namespace Interval_Timer
             ButtonCloseMenu.Visibility = Visibility.Visible;
         }
 
-        private void Dt_Tick(object sender, EventArgs e)
-        {
-            if (sw.IsRunning)
-            {
-                TimeSpan ts = sw.Elapsed;
-                currentTime = String.Format("{0:00}:{1:00}:{2:00}",
-                ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
-                Stopwatch.Text = currentTime;
-            }
-            else
-            {
-                Stopwatch.Text = "{Binding currentTime, StringFormat={}";
-            }
-        }
-
-        int clicker = 0;
-        private void StartStop_Click(object sender, RoutedEventArgs e)
-        {
-            if (clicker % 2 != 0)
-            {
-                sw.Stop();
-                dt.Stop();
-                StartStop.Content = "Resume";
-                ResetOrLap.Content = "Reset";
-            }
-            else
-            {
-                sw.Start();
-                dt.Start();
-                StartStop.Content = "Pause";
-                ResetOrLap.Content = "Lap";
-            }
-            clicker++;
-        }
-
-        private void ResetOrLap_Click(object sender, RoutedEventArgs e)
-        {
-            Stopwatch.Text = "0:00:00";
-        }
-
         private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
         {
             DragMove();
@@ -108,9 +64,30 @@ namespace Interval_Timer
         {
             int index = ListViewMenu.SelectedIndex;
             MoveCursorMenu(index);
-        }
 
-        private void MoveCursorMenu(int index)
+            switch (index)
+            {
+                case 0:
+                    MainGrid.Children.Clear();
+                    ///MainGrid.Children.Add(new StopwatchGrid());
+                    break;
+                case 1:
+                    MainGrid.Children.Clear();
+                    ///MainGrid.Children.Add(new StopwatchGrid());
+                    break;
+                case 2:
+                    MainGrid.Children.Clear();
+                    ///MainGrid.Children.Add(new StopwatchGrid());
+                    break;
+                case 3:
+                    MainGrid.Children.Clear();
+                    MainGrid.Children.Add(new StopwatchGrid());
+                    break;
+                default:
+                    break;
+            }
+        }
+            private void MoveCursorMenu(int index)
         {
             TransitioningContentSlide.OnApplyTemplate();
             GridCursor.Margin = new Thickness(0, 60 * index, 0, 0);
---
+++ Interval Timer/Properties/StopwatchGrid.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Diagnostics;
+
+namespace Interval_Timer.Properties
+{
+    /// <summary>
+    /// Interaction logic for StopwatchGrid.xaml
+    /// </summary>
+    public partial class StopwatchGrid : UserControl
+    {
+        DispatcherTimer dt = new DispatcherTimer();
+        Stopwatch sw = new Stopwatch();
+        string currentTime = string.Empty;
+        public StopwatchGrid()
+        {
+            InitializeComponent();
+            dt.Tick += new EventHandler(Dt_Tick);
+            dt.Interval = new TimeSpan(0, 0, 0, 0, 1);
+        }
+
+        private void Dt_Tick(object sender, EventArgs e)
+        {
+            if (sw.IsRunning)
+            {
+                TimeSpan ts = sw.Elapsed;
+                currentTime = String.Format("{0:00}:{1:00}:{2:00}",
+                ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+                Stopwatch.Text = currentTime;
+            }
+            else
+            {
+                Stopwatch.Text = "{Binding currentTime, StringFormat={}";
+            }
+        }
+
+        int clicker = 0;
+        private void StartStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (clicker % 2 != 0)
+            {
+                sw.Stop();
+                dt.Stop();
+                StartStop.Content = "Resume";
+                ResetOrLap.Content = "Reset";
+            }
+            else
+            {
+                sw.Start();
+                dt.Start();
+                StartStop.Content = "Pause";
+                ResetOrLap.Content = "Lap";
+            }
+            clicker++;
+        }
+
+        private void ResetOrLap_Click(object sender, RoutedEventArgs e)
+        {
+            Stopwatch.Text = "0:00:00";
+        }
+    }
+}
